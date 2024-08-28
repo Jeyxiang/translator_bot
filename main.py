d94918454a622d2722985cad76f8ccab3923e2fc
jeyxiang <jeyrex99@gmail.com>
2024-08-28 21:26:10 +0800

    added sqlitedb

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, Update,
 from telegram.ext import (Application, MessageHandler, CommandHandler, filters, ConversationHandler,
                           CallbackQueryHandler, ContextTypes)
 
+import sqlite3
+
 ## Where token id is stored
-from credentials import TOKEN
+from credentials import TOKEN, DB
 from constants import LANGUAGE_TO_CODE
 from deep_translator import GoogleTranslator
 
@@ -13,6 +15,50 @@ SRC, TARGET = 'english', 'chinese'
 
 # Define states
 SRC_TYPE, TARGET_TYPE = range(2)
+
+def init_db(user_id):
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS userinfo (
+            id INTEGER PRIMARY KEY,
+            src TEXT NOT NULL,
+            tgt TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+    update_configurations(user_id, 'english', 'chinese')
+    
+    return True
+
+
+def update_configurations(user_id, source, target):
+    # reopen connection
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    if len(cur.execute(f'''SELECT id FROM userinfo WHERE id = {user_id}''').fetchall()) > 0:
+        # update user config
+        cur.execute(f'''UPDATE userinfo SET src = "{source}", tgt = "{target}" WHERE id = {user_id}''')
+    else:
+        # insert values if user_id not found
+        cur.execute(f'''INSERT INTO userinfo (id, src, tgt) VALUES ({user_id},"{source}","{target}")''')
+    conn.commit()
+    conn.close()
+    return True
+
+def get_configurations(user_id):
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    cur.execute(f'''SELECT src, tgt FROM userinfo WHERE id = {user_id}''')
+    row = cur.fetchone()
+    conn.close()
+    if row:
+        src, tgt = row
+        return (src, tgt)
+    else:
+        return ('auto','english')
 
 
 def default_keyboard() -> ReplyKeyboardMarkup:
@@ -28,13 +74,13 @@ def default_keyboard() -> ReplyKeyboardMarkup:
 # COMMANDS
 async def source_type(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Stores the source language."""
-    global SRC
     query = update.callback_query
     await query.answer()
-    SRC = query.data
+    context.user_data['SRC'] = query.data
+
     # logger.info('Car type of %s: %s', user.first_name, update.message.text)
     await query.edit_message_text(
-        f'You selected <b>{SRC}</b> as source.\n'
+        f'You selected <b>{query.data}</b> as source.\n'
         f'What language do you want to translate to?',
         parse_mode='HTML'
     )
@@ -47,28 +93,35 @@ async def source_type(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
 
 async def target_type(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Stores the target language"""
-    global TARGET
     query = update.callback_query
     await query.answer()
-    TARGET = query.data
-    if TARGET == 'auto':
-        TARGET = 'english'
+    src_language = context.user_data.get('SRC')
+    target_language = query.data
+    if src_language == 'auto':
+        src_language = 'english'
     await query.edit_message_text(
         text=f'You selected <b>{query.data}</b> as your target language.',
         parse_mode='HTML'
     )
+
+    ## UPDATE DB
+    user_id = update.effective_user.id
+    update_configurations(user_id, src_language, target_language)
+
     message = f'To view your current configurations, use /summary\n To edit your configurations, use /configure'
     chat_id = update.effective_chat.id
     await context.bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML', reply_markup=default_keyboard())
-    return ConversationHandler.END 
+    return ConversationHandler.END
 
 
 async def summary_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Summarizes the user's selections and ends the conversation"""
+    user_id = update.effective_user.id
+    src, target = get_configurations(user_id)
     # Construct the summary text
     summary_text = (f"Here's your current settings:\n"
-                    f"<b>Source language:</b> {SRC}\n"
-                    f"<b>Target language:</b> {TARGET}\n"
+                    f"<b>Source language:</b> {src}\n"
+                    f"<b>Target language:</b> {target}\n"
                     f"<b>Happy Translating!</b>")
     chat_id = update.effective_chat.id
     await context.bot.send_message(chat_id=chat_id, text=summary_text, parse_mode='HTML', reply_markup=default_keyboard())
@@ -97,6 +150,10 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
 
 async def start_command(update, context):
+    # insert user id into DB 
+    user_id = update.effective_user.id
+    init_db(user_id)
+
     await update.message.reply_text(
         f'<b>Welcome to Translation Bot!</b>',
         parse_mode='HTML',
@@ -113,13 +170,15 @@ async def help_command(update, context):
     )
 
 
-
 async def translate_command(update, context):
     text_to_translate = update.message.text
-    text_translated = GoogleTranslator(source=LANGUAGE_TO_CODE[SRC], target=LANGUAGE_TO_CODE[TARGET]).translate(text_to_translate)
+    user_id = update.effective_user.id
+    print(f"User ID: {user_id} is translating")
+
+    src, target = get_configurations(user_id)
+    text_message = GoogleTranslator(source=LANGUAGE_TO_CODE[src], target=LANGUAGE_TO_CODE[target]).translate(text_to_translate)
     await update.message.reply_text(
-        f'Translated: "{text_to_translate}" \n'
-        f'to {TARGET}: {text_translated}',
+        text_message,
         parse_mode='HTML',
         reply_markup = default_keyboard()
     )
@@ -136,7 +195,6 @@ def main() -> None:
     token = TOKEN
     application = Application.builder().token(token).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
     
-
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('configure', configure_command)],
         states={
